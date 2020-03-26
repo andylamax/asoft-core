531022c07da14af9404b4d79a19c0480eb7fa4c9
andylamax <andylamax@programmer.net>
2020-03-26 04:43:37 +0300

    Added subprojects

---
+++ buildSrc/src/main/kotlin/versions.kt
@@ -0,0 +1,24 @@
+object versions {
+    const val kotlin = "1.4-M1"
+    const val selenium = "4.0.0-alpha-4"
+
+    object asoft {
+        const val test = "4.2.1-1.4-M1"
+    }
+
+    object kotlinx {
+        const val coroutines = "1.3.5-1.4-M1"
+        const val serialization = "0.20.0-1.4-M1"
+    }
+
+    object android {
+        const val build_tools = "3.6.0"
+    }
+
+    object androidx {
+        const val appcompat = "1.1.0"
+        const val espresso = "3.2.0"
+        const val test_runner = "1.2.0"
+        const val test_rules = "1.2.0"
+    }
+}
