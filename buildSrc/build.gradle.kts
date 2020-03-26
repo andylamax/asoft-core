ca229261e9c3b3cc70cf7dc9a2c859f0e7130162
andylamax <andylamax@programmer.net>
2020-03-26 05:50:52 +0300

    Releasing v0.0.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,9 +3,19 @@ plugins {
 }
 
 repositories {
+    google()
     jcenter()
+    maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
+}
+
+object versions {
+    val kotlin = "1.4-M1"
+
+    object android {
+        const val build_tools = "3.6.0"
+    }
 }
 
 dependencies {
-
+    api("com.android.tools.build:gradle:${versions.android.build_tools}")
 }
