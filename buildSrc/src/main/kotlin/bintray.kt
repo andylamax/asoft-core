5be3a0ee9896e99987ce03936e5ccb08e60696a5
andylamax <andylamax@programmer.net>
2020-04-21 04:02:53 +0300

    Released 1.0.0 with gradle metadata

--- buildSrc/src/main/kotlin/bintray.kt
+++ buildSrc/src/main/kotlin/bintray.kt
@@ -1,6 +1,27 @@
 import com.jfrog.bintray.gradle.BintrayExtension
+import com.jfrog.bintray.gradle.tasks.BintrayUploadTask
 import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.publish.maven.internal.artifact.FileBasedMavenArtifact
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
 import java.util.*
+
+fun Project.addModuleJson() = tasks.withType<BintrayUploadTask> {
+    doFirst {
+        project.extensions.getByType<PublishingExtension>().publications
+            .filterIsInstance<MavenPublication>()
+            .forEach { publication ->
+                val moduleFile = buildDir.resolve("publications/${publication.name}/module.json")
+                if (moduleFile.exists()) {
+                    publication.artifact(object : FileBasedMavenArtifact(moduleFile) {
+                        override fun getDefaultExtension() = "module"
+                    })
+                }
+            }
+    }
+}
 
 fun BintrayExtension.configureBintray(project: Project) {
     val props = Properties().apply {
@@ -8,6 +29,7 @@ fun BintrayExtension.configureBintray(project: Project) {
     }
     user = props["bintray.user"] as String
     key = props["bintray.key"] as String
+    project.addModuleJson()
     publish = true
     setPublications("kotlinMultiplatform", "metadata", "androidRelease", "jvm", "js")
     pkg = PackageConfig().apply {
